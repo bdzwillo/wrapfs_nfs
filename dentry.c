96ba48ce2547823861c093c142b8d1138fce80bb
Barnim Dzwillo <bd@strato-rz.de>
2020-10-27 13:13:17 +0100

    wrapfs: added d_weak_revalidate wrapper for nfs overlay

--- dentry.c
+++ dentry.c
@@ -13,25 +13,57 @@
 
 /*
  * returns: -ERRNO if error (returned to user)
- *          0: tell VFS to invalidate dentry
+ *          0: tell VFS to invalidate dentry (calls d_invalidate(dentry) there)
  *          1: dentry is valid
  */
 static int wrapfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 {
 	struct dentry *lower_dentry;
-	int err = 1;
+	int ret = 1;
 
-	if (flags & LOOKUP_RCU)
-		return -ECHILD;
-
+	/* If in rcu-walk mode, the filesystem must revalidate the dentry without
+	 * blocking or storing to the dentry, d_parent and d_inode should not be
+	 * used without care (because they can change and, in d_inode case, even
+	 * become NULL under us)
+	 * 
+	 * This might even be the case for wrapfs_get_lower_dentry() where
+	 * dentry->d_fsdata can be NULL here.
+	 */
+	if (flags & LOOKUP_RCU) {
+		return -ECHILD; // call d_revalidate() again in ref-walk-mode
+	}
 	lower_dentry = wrapfs_get_lower_dentry(dentry);
 	if (!(lower_dentry->d_flags & DCACHE_OP_REVALIDATE))
 		goto out;
-	err = lower_dentry->d_op->d_revalidate(lower_dentry, flags);
+	ret = lower_dentry->d_op->d_revalidate(lower_dentry, flags);
 
-	pr_debug("wrapfs: revalidate(%pd4, 0x%04x) = %d", dentry, flags, err);
+	pr_debug("wrapfs: revalidate(%pd4, 0x%04x) = %d", dentry, flags, ret);
+
+	if (ret < 0) {
+		goto out; // might also be ECHILD to call revalidate again
+	}
 out:
-	return err;
+	return ret;
+}
+
+/*
+ * weak_revalidate is called for result of path lookup (namei:complete_walk())
+ * 0: returns ESTALE there
+ * 1: dentry is valid
+ */
+static int wrapfs_d_weak_revalidate(struct dentry *dentry, unsigned int flags)
+{
+	struct dentry *lower_dentry;
+	int ret = 1;
+
+	lower_dentry = wrapfs_get_lower_dentry(dentry);
+	if (!(lower_dentry->d_flags & DCACHE_OP_WEAK_REVALIDATE))
+		goto out;
+	ret = lower_dentry->d_op->d_weak_revalidate(lower_dentry, flags);
+
+	pr_debug("wrapfs: weak_revalidate(%pd4, 0x%04x) = %d", dentry, flags, ret);
+out:
+	return ret;
 }
 
 static void wrapfs_d_release(struct dentry *dentry)
@@ -49,4 +81,5 @@ static void wrapfs_d_release(struct dentry *dentry)
 const struct dentry_operations wrapfs_dops = {
 	.d_revalidate	= wrapfs_d_revalidate,
 	.d_release	= wrapfs_d_release,
+	.d_weak_revalidate = wrapfs_d_weak_revalidate,
 };
