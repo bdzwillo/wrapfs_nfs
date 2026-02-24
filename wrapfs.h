c5fcef5db17c2cd0c17c7042d1f4eaf805a049e3
Barnim Dzwillo <dzwillo@strato.de>
2026-02-24 15:00:12 +0100

    wrapfs: since 6.3 file locking definitions are in filelock.h header file
    This change was also backported to the redhat 9.7 kernel-5.14
    
    see: filelock: move file locking definitions to separate header file
         https://github.com/torvalds/linux/commit/5970e15dbcfeb0ed3a0bf1954f35bbe60a048754

--- wrapfs.h
+++ wrapfs.h
@@ -34,6 +34,8 @@
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
 #include <linux/filelock.h>
+#elif defined(RHEL_MAJOR) && RHEL_MAJOR == 9 && defined(RHEL_MINOR) && RHEL_MINOR >= 7 && LINUX_VERSION_CODE >= KERNEL_VERSION(5, 14, 0)
+#include <linux/filelock.h>
 #endif
 
 /* the file system name */
